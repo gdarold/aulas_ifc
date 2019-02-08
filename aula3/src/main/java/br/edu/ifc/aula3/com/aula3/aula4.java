8c04a7f03c8a4f51cb947254096f37874f054e90
aluno <aluno@lab1-pc02.local>
2019-02-08 19:16:23 -0200

    08.02 first

---
+++ aula3/src/main/java/br/edu/ifc/aula3/com/aula3/aula4.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifc.aula3.com.aula3;
+
+/**
+ *
+ * @author aluno
+ */
+public class aula4 {
+    
+    public static void main(String[] main){
+        
+        String[] semana = {"Domingo", "Segunda", "Terça", "quarta", "quinta", "Sexta", "Sábado"
+                    
+        };
+        
+        
+        for (String string : semana) {
+            
+            System.out.println(string);
+            
+        }
+        
+        
+        
+    }
+    
+    
+    
+}
